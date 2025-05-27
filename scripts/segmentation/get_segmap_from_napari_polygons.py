8655f1631ecc91160365318cc3ce6fa50c148a79
emoebel <emmanuel.moebel@gmail.com>
2025-05-27 11:16:39 +0200

    the script works now - seg maps are being generated. I encapsulated code from script in functions and stored them into utils.io

--- scripts/segmentation/get_segmap_from_napari_polygons.py
+++ scripts/segmentation/get_segmap_from_napari_polygons.py
@@ -1,8 +1,6 @@
-import os
 import utils.io as io
-import pandas as pd
-import numpy as np
 import napari.layers
+import numpy as np
 
 path_polygons = '/Users/manu/boulot/unit_solutions/annotations/segmentation/napari_polygons/'
 path_images = '/Users/manu/boulot/unit_solutions/data/all/'
@@ -15,42 +13,74 @@ fname = fname_list[0]
 
 path_csv_file = path_polygons + fname + '.csv'
 
-# Prepare data argument for napari.layers.Shapes construcotr
-df = pd.read_csv(path_csv_file)
+# # Prepare data argument for napari.layers.Shapes construcotr
+# df = pd.read_csv(path_csv_file)
+#
+# polygon_idx_list = list(set(df['index']))
+# polygon_list = []
+# for polygon_idx in polygon_idx_list:
+#     df_polygon = df.loc[df['index']==polygon_idx]
+#     polygon_vertex_coord_list = []
+#     for idx, row in df_polygon.iterrows():
+#         polygon_vertex_coord_list.append([row['axis-0'], row['axis-1']])
+#
+#     polygon_list.append(polygon_vertex_coord_list)
 
-polygon_idx_list = list(set(df['index']))
-polygon_list = []
-for polygon_idx in polygon_idx_list:
-    df_polygon = df.loc[df['index']==polygon_idx]
-    polygon_vertex_coord_list = []
-    for idx, row in df_polygon.iterrows():
-        polygon_vertex_coord_list.append([row['axis-0'], row['axis-0']])
-
-    polygon_list.append(polygon_vertex_coord_list)
+data = io.get_data_for_shapes_layer_from_polygon_csv(path_csv_file)
 
 # Create Shapes object:
 shapes_layer = napari.layers.Shapes(
-    data=polygon_list,
+    data=data,
     shape_type='polygon',
 )
 
-# Get image shape for Shapes.to_labels:
-from PIL import Image
-def open_img_as_np_array(fname):
-    return np.asarray(Image.open(fname))
 
-img = open_img_as_np_array(path_images + fname + '.jpg')
+# from napari_builtins.io import read_csv
+# data, column_names, layer_type = read_csv(path_csv_file)
+#
+# # Create Shapes object:
+# shapes_layer = napari.layers.Shapes(
+#     data=data,
+#     shape_type='polygon',
+# )
+
+# Get image shape for Shapes.to_labels:
+# from PIL import Image
+# def open_img_as_np_array(fname):
+#     return np.asarray(Image.open(fname))
+
+img = io.open_img_as_np_array(path_images + fname + '.jpg')
 img_shape = img.shape[:2]
 
 # Get segmentation map from Shapes object:
 seg_map = shapes_layer.to_labels(
     labels_shape=img_shape,
 )
-seg_map.shape
-np.unique(seg_map)
+# seg_map.shape
+# np.unique(seg_map)
+#
+# seg_map = shapes_layer.to_masks(
+#     mask_shape=img_shape,
+# )
+# seg_map.shape
+# np.unique(seg_map)
 
-seg_map = shapes_layer.to_masks(
-    mask_shape=img_shape,
-)
-seg_map.shape
-np.unique(seg_map)
+
+
+# data = [
+#     [[50.2, 50], [50, 100], [100, 100], [100, 50], [80, 50]],
+# ]
+#
+# # Create Shapes object:
+# shapes_layer = napari.layers.Shapes(
+#     data=data,
+#     shape_type='polygon',
+# )
+#
+# # Get segmentation map from Shapes object:
+# seg_map = shapes_layer.to_labels(
+#     labels_shape=[200, 200],
+# )
+
+import matplotlib.pyplot as plt
+plt.imsave('seg_map.png', seg_map)  # for visu
--- utils/io.py
+++ utils/io.py
@@ -1,4 +1,7 @@
 import os.path
+import pandas as pd
+from PIL import Image
+import numpy as np
 
 def get_fname_list_from_dir(dir_path, ext='csv'):
     # Get all files in directory:
@@ -6,7 +9,31 @@ def get_fname_list_from_dir(dir_path, ext='csv'):
     fname_list = []
     for fname in dir_fnames:
         if fname[0] != '.' and os.path.splitext(fname)[1] == '.' + ext:
-            fname_list.append(fname)
+            fname_without_extension = os.path.splitext(fname)[0]
+            fname_list.append(fname_without_extension)
     fname_list.sort()
 
     return fname_list
+
+
+def get_data_for_shapes_layer_from_polygon_csv(fname_csv):
+    """
+    :param fname_csv: (str) file name of csv file, as created by saving Napari shapes layer
+    :return data: (list) List of shape data, where each element is an (N, D) array of the N vertices of a shape in D dimensions. As needed by napari.layers.Shapes constructor.
+    """
+    df = pd.read_csv(fname_csv)
+
+    polygon_idx_list = list(set(df['index']))
+    data = []
+    for polygon_idx in polygon_idx_list:
+        df_polygon = df.loc[df['index'] == polygon_idx]
+        polygon_vertex_coord_list = []
+        for idx, row in df_polygon.iterrows():
+            polygon_vertex_coord_list.append([row['axis-0'], row['axis-1']])
+
+        data.append(polygon_vertex_coord_list)
+    return data
+
+
+def open_img_as_np_array(fname):
+    return np.asarray(Image.open(fname))
