d85f1c88d59fbefa11535e50a337108350b1dd4f
emoebel <emmanuel.moebel@gmail.com>
2025-06-26 16:36:42 +0200

    added setup.py and pyproject.toml

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fhand:
+    long_description = fhand.read()
+
+setuptools.setup(
+    name="signreader",
+    version="0.0.1",
+    author="Emmanuel Moebel",
+    author_email="emmanuel.moebel@gmail.com",
+    description=("AI-based tool for extracting information from photos of hiking signs."),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Intended Audience:: Developers",
+        "Programming Language:: Python",
+        "Programming Language:: Python:: 3",
+        "Programming Language:: Python:: 3.10",
+        "Operating System:: OS Independent",
+    ],
+    install_requires=[
+        "cellpose==4.0.4",
+        "ultralytics==8.3.156",
+        "moondream==0.1.0",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.10",
+    entry_points={
+        "console_scripts": [
+            "signreader = signreader.__main__:main",
+        ]
+    }
+)
